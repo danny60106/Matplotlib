52d73e9fbab9ade838119f52b0d701cf11a1bbbe
danny60106 <54652877+danny60106@users.noreply.github.com>
2020-07-10 10:46:07 +0800

    Update plt2_figure.py

--- plt2_figure.py
+++ plt2_figure.py
@@ -3,15 +3,18 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-x = np.linspace(-3, 3, 50)
+x = np.linspace(-3, 3, 50) 
+#從-3~3生成50個點來畫
+
 y1 = 2*x + 1
 y2 = x**2
 
 plt.figure()
 plt.plot(x, y1)
 
-plt.figure(num=3, figsize=(8, 5),)
+plt.figure(figsize=(8, 5))
 plt.plot(x, y2)
+
 # plot the second curve in this figure with certain parameters
 plt.plot(x, y1, color='red', linewidth=1.0, linestyle='--')
 plt.show()
